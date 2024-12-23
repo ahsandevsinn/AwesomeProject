fa80d6a1fb7a302a188ecac4e27fe91a002a1d07
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-12-23 01:48:21 -0800

    js code added

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { View , Text, Button} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+function App() {
+  return (
+    <View>
+     <Text style={{fontSize:40}}> Hello World</Text>
+     <Text style={{fontSize:40}}> Like</Text>
+     <Text style={{fontSize:40}}> Subscribe</Text>
+     <Text style={{fontSize:40}}> Share</Text>
+     <Button title='Press Here'> </Button>
+    </View>
+    
+  )
+}
+
+
+export default App;
