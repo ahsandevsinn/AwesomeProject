0f15c32d067d7477aed83ef6a96e9930cf54d2c3
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-12-23 03:56:47 -0800

    added use state

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import { UserData } from './components/app_components';
 
-const Data = (value)=>{
+const Data = (value) => {
   return console.warn(value)
 }
 
 function App() {
+  const [name, setName] = useState("Hello");
+
+  function changeName() {
+    setName("Hello Ahsan");
+  }
   return (
     <View >
-      <Button title='Press Here' onPress={()=>{Data("Hello Ahsan How Are you")}}> </Button>
+    <Text style={{fontSize:20}}>{name}</Text>
+      <Button title='Press Here' onPress={() => { changeName() }}> </Button>
     </View>
-
   )
 
 }
