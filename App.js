17d29a41767fa6e7aace525725ad951fdb00829d
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-12-23 02:57:45 -0800

    add function and function parameter

--- App.js
+++ App.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { UserData } from './components/app_components';
 
+const Data = (value)=>{
+  return console.warn(value)
+}
+
 function App() {
   return (
-    <View>
-      <UserData />
+    <View >
+      <Button title='Press Here' onPress={()=>{Data("Hello Ahsan How Are you")}}> </Button>
     </View>
 
   )
