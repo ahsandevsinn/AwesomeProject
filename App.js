6547040ba77942ca07f73af0ab51f5b81e0878ab
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-12-24 02:38:40 -0800

    fetch api call

--- App.js
+++ App.js
@@ -1,35 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TextInputComponent, TextInput, Button } from 'react-native';
 import appStyles from './styles/style';
 // import appStyles from './styles/style';
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const [data,setData] = useState(undefined);
+
+const getApi = async ()=>{
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  let result = await fetch(url);
+  result = await result.json();
+  setData(result);
+  
+}
+  useEffect(()=>{
+   getApi();
+  },[])
 
   return (
     <View style={appStyles.View}>
-      <Text style={appStyles.textBox}>Login Form</Text>
-      <TextInput style={appStyles.textInput} placeholder='Enter user name' onChangeText={(text)=>setName(text)} value={name}></TextInput>
-      <TextInput style={appStyles.textInput} placeholder='Enter user email' onChangeText={(text)=>setEmail(text)} value={email}></TextInput>
-      <TextInput style={appStyles.textInput} placeholder='Enter user password' onChangeText={(text)=>setPassword(text)} value={password}></TextInput>
-      <View style={{ width: 200, borderRadius: 30, padding: 10 }}>
+      <Text style={appStyles.textBox}>Fetch API</Text>
+     data ?  <Text style={appStyles.textBox}>{data[0].id}</Text> : null;
+     data ?  <Text style={appStyles.textBox}>{data[0].title}</Text> : null;
+     data ?  <Text style={appStyles.textBox}>{data[0].body}</Text> : null;
 
-        <Button title='Save Data' onPress={()=>{
-        
-          console.table([name,email,password]);
-          
-        }}></Button>
-      </View>
-      <View style={{ width: 200, borderRadius: 30, padding: 5 }}>
-
-        <Button title='Clear Data' onPress={()=>{
-          setEmail('');
-          setName('');
-          setPassword('');
-        }}></Button>
-      </View>
+    
     </View>
   );
 }
